18214b4608a737a16efb8a2c841df2aa16938cdf
Leonardo <leonardo.mdo90@gmail.com>
2021-01-24 17:32:06 -0300

    Testando a classe de demonio - Categoria

--- src/main/java/leo/com/cursomc/resources/CategorieResource.java
+++ src/main/java/leo/com/cursomc/resources/CategorieResource.java
@@ -1,9 +1,13 @@
 package leo.com.cursomc.resources;
 
+import leo.com.cursomc.domain.Categorie;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @ComponentScan(basePackages = "leo.com.cursomc.resources")
@@ -11,7 +15,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategorieResource {
 
     @RequestMapping(method=RequestMethod.GET)
-    public String list(){
-        return "Rest esta funcionando!";
+    public List<Categorie> list(){
+
+        Categorie cat1 = new Categorie(1, "Informatica");
+        Categorie cat2 = new Categorie(2,"Escritorio");
+
+        List<Categorie> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }
